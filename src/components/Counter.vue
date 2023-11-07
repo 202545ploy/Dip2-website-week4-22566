6e4474e173f6ab90842967d34e87e2c9488d8281
Khunaiko <pasuaiko21089@gmail.com>
2023-11-07 11:39:08 +0700

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    name:'CounTer',
+    data() {
+        return {
+            Counter:0
+        }
+    },
+    methods: {
+        submiFrom(event) {
+            // prevent a page refresh
+            event.preventDefault();
+            alert('The from was sumitted')
+        },
+        countUp() {
+            this.Counter++   //this.Counter = this.Counter + 1
+        },
+        countDown() {
+            this.Counter--   //this.Counter = this.Counter - 1
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>{{ Counter }}</h1>
+        <span>
+            <button @click.right="countUp">Counter UP</button>
+            <button @click.middle="countDown">Counter Down</button>
+        </span>
+         
+    </div>
+    <from @submit.prevent="submiFrom">
+        <p>
+            <button>Submit</button>
+        </p>
+    </from>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/EventHandle.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+       <!-- <h1>Week3:Event Handling</h1>
+        <hr />
+        <p>{{ name }} {{ ego }}</p>
+        <button @click="changeName('Elon Musk'), changeEgo('Starlord')">Chanae name</button>  -->
+        <p> Name:{{ name }} </p>
+        <input type="text" @input="getInput($event,'Tommy', 50)">
+    </div> 
+</template>
+
+<script>
+export default {
+    name: 'EventHandle',
+    data() {
+        return{
+            name: "",
+            ego: "Actor",
+        }
+    },
+    methods: {
+        changeName(nameParams) {
+            this.name = nameParams
+        },
+        changeEgo(egoParams) {
+            this.ego = egoParams
+        },
+        getInput(event,lastname, age) {
+            this.name = event.target.value + ' ' + lastname + ' ' + age
+            console.log(event, lastname)
+        }
+    }
+}
+</script>
+
